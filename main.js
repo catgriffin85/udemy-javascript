30341db1f729cb41f328e896cdb40b84d671e3b6
Catherine Griffin <catherine.griffin@students.codeinstitute.net>
2024-07-15 21:10:20 +0000

    Add event for nav menu

---
+++ main.js
@@ -0,0 +1,14 @@
+
+document.querySelector('#open-nav-menu').addEventListener('click', function (){
+    document.querySelector('header nav .wrapper').classList.add('nav-open');
+});
+
+document.querySelector('#close-nav-menu').addEventListener('click', function (){
+    document.querySelector('header nav .wrapper').classList.remove('nav-open');
+});
+
+const customer = 'Sara';
+let balance = 2000;
+
+
+
